af658411698f1a2bd2e6420061666c570118e1cc
Marco De Jesus <marco_dejesus@live.com>
2021-04-28 04:08:44 -0500

    Implementando la Suma de numeros pares.

---
+++ src/SumaNumerosPares/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumaNumerosPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 4;
+            int suma = 2;
+
+            while(numero <= 10){
+                suma = suma + numero;
+                numero = numero + 2;
+            }
+
+            Console.WriteLine("Suma pares entre 2 y 10 = " + suma);
+        }
+    }
+}
